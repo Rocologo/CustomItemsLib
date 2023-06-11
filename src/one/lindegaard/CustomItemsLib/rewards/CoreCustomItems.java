0e468bdaa351d98eaa9370df158e72bb2caad382
christian <christian@carbonx1>
2023-06-11 11:01:29 +0200

    First MC 1.20 build

--- src/one/lindegaard/CustomItemsLib/rewards/CoreCustomItems.java
+++ src/one/lindegaard/CustomItemsLib/rewards/CoreCustomItems.java
@@ -38,6 +38,7 @@ import one.lindegaard.Core.v1_18_R1.Skins_1_18_R1;
 import one.lindegaard.Core.v1_19_R1.Skins_1_19_R1;
 import one.lindegaard.Core.v1_19_R2.Skins_1_19_R2;
 import one.lindegaard.Core.v1_19_R3.Skins_1_19_R3;
+import one.lindegaard.Core.v1_19_R3.Skins_1_20_R1;
 import one.lindegaard.Core.v1_8_R1.Skins_1_8_R1;
 import one.lindegaard.Core.v1_8_R2.Skins_1_8_R2;
 import one.lindegaard.Core.v1_8_R3.Skins_1_8_R3;
@@ -77,7 +78,9 @@ public class CoreCustomItems {
 			return null;
 		}
 		// https://www.spigotmc.org/wiki/spigot-nms-and-minecraft-versions-1-16/
-		if (version.equals("v1_19_R3")) {
+		if (version.equals("v1_20_R1")) {
+			sk = new Skins_1_20_R1();
+		} else if (version.equals("v1_19_R3")) {
 			sk = new Skins_1_19_R3();
 		} else if (version.equals("v1_19_R2")) {
 			sk = new Skins_1_19_R2();
